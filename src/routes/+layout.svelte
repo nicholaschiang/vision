e58086d5119808a00679f7b5da12f3697b0339b7
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2025-02-05 20:14:20 -0700

    feat: only use Tailwind for styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 	let { children } = $props();
 </script>
 
-<div class="bg-gray-950 text-gray-50 min-h-screen">
+<div class="min-h-screen bg-gray-50 text-gray-950 dark:bg-gray-950 dark:text-gray-50">
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,102 +2,75 @@
 	import posts from '../posts.json';
 	import collections from '../collections.json';
 
+	import Button from '$lib/components/Button.svelte';
+
+	import { Plus, Minus } from 'lucide-svelte';
+
+	let gridCols = $state(4);
 	let selectedCollectionIds = $state([collections[0].collection_id]);
-	let filteredPosts = $derived(posts.filter((post) => post.saved_collection_ids.some((collectionId) => selectedCollectionIds.includes(collectionId))));
+	let filteredPosts = $derived(
+		posts.filter(
+			(post) =>
+				selectedCollectionIds.includes('ALL_MEDIA_AUTO_COLLECTION') ||
+				post.saved_collection_ids.some((collectionId) =>
+					selectedCollectionIds.includes(collectionId)
+				)
+		)
+	);
 </script>
 
-<div class="flex flex-col gap-2 py-2">
-	<div class="filter-group">
-		{#each collections as collection (collection.collection_id)}
-			<button
-				class="filter"
-				data-state={selectedCollectionIds.includes(collection.collection_id) ? 'selected' : null}
-				onclick={() => {
-					if (selectedCollectionIds.includes(collection.collection_id)) {
-						selectedCollectionIds = selectedCollectionIds.filter(
-							(id) => id !== collection.collection_id
-						);
-					} else {
-						selectedCollectionIds = [...selectedCollectionIds, collection.collection_id];
-					}
-				}}
-			>
-				{collection.collection_name}
-			</button>
-		{/each}
-	</div>
-	<div class="grid grid-cols-4 gap-2 px-2">
-		{#each filteredPosts as post}
-			<a
-				aria-label={post.caption?.text}
-				href={`https://instagram.com/p/${post.code}`}
-				target="_blank"
-				rel="noopener noreferrer"
-				class="aspect-4/5"
-			>
-				<img
-					src={`/media?url=${encodeURIComponent(post.image_versions2.candidates[0].url)}`}
-					alt={post.caption?.text}
-					class="h-full w-full object-cover"
-				/>
-			</a>
-		{/each}
-	</div>
+<div class="flex flex-wrap gap-2 p-2">
+	{#each collections as collection (collection.collection_id)}
+		<Button
+			data-active={selectedCollectionIds.includes(collection.collection_id) ? '' : undefined}
+			onclick={() => {
+				if (selectedCollectionIds.includes(collection.collection_id)) {
+					selectedCollectionIds = selectedCollectionIds.filter(
+						(id) => id !== collection.collection_id
+					);
+				} else {
+					selectedCollectionIds = [...selectedCollectionIds, collection.collection_id];
+				}
+			}}
+		>
+			{collection.collection_name}
+		</Button>
+	{/each}
 </div>
 
-<style>
-	:root {
-		--mono3: #232323;
-		--mono4: #282828;
-		--mono5: #1a1a1a;
-		--mono8: #505050;
-		--mono11: #a0a0a0;
-		--mono12: #ededed;
-	}
-	.filter-group,
-	.filter {
-		display: flex;
-		align-items: center;
-	}
-	.filter-group {
-		flex-wrap: wrap;
-		row-gap: 8px;
-	}
-	.filter {
-		--height: 36px;
-		z-index: 1;
-		-webkit-user-select: none;
-		-moz-user-select: none;
-		user-select: none;
-		cursor: pointer;
-		height: var(--height);
-		border-radius: var(--height);
-		padding: 0 16px;
-		border: 1px solid var(--mono5);
-		justify-content: center;
-		color: var(--mono11);
-		text-wrap: nowrap;
-		font-size: 14px;
-		transition: 100ms;
-		transition-property: margin, padding, border-color, border-radius, background, color;
-	}
-	.filter {
-		z-index: 0;
-		margin-left: 8px;
-	}
-	.filter[data-state='selected'] {
-		z-index: 1;
-		background: var(--mono4);
-		border-color: var(--mono8);
-		color: var(--mono12);
-	}
-	.filter[data-state='selected'] + .filter[data-state='selected'] {
-		z-index: 0;
-		margin-left: -16px;
-		padding-left: 28px;
-		border-top-left-radius: 0;
-		border-bottom-left-radius: 0;
-		border-left-color: transparent;
-		background: var(--mono3);
-	}
-</style>
+<div class="flex gap-2 p-2">
+	<Button
+		class="flex h-6 w-6 items-center justify-center rounded-full p-0"
+		onclick={() => {
+			gridCols--;
+		}}
+	>
+		<Plus class="h-4 w-4" />
+	</Button>
+	<Button
+		class="flex h-6 w-6 items-center justify-center rounded-full p-0"
+		onclick={() => {
+			gridCols++;
+		}}
+	>
+		<Minus class="h-4 w-4" />
+	</Button>
+</div>
+
+<div class="grid gap-2 p-2" style:grid-template-columns={`repeat(${gridCols}, minmax(0, 1fr))`}>
+	{#each filteredPosts as post}
+		<a
+			aria-label={post.caption?.text}
+			href={`https://instagram.com/p/${post.code}`}
+			target="_blank"
+			rel="noopener noreferrer"
+			class="aspect-4/5"
+		>
+			<img
+				src={`/media?url=${encodeURIComponent(post.image_versions2.candidates[0].url)}`}
+				alt={post.caption?.text}
+				class="h-full w-full object-cover"
+			/>
+		</a>
+	{/each}
+</div>
