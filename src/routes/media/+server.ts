f3dd89340052669940f5ddf107ffc8b353126bfd
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2025-02-05 21:27:24 -0700

    perf: cache downloaded media on disk

--- src/routes/media/+server.ts
+++ src/routes/media/+server.ts
@@ -1,15 +1,39 @@
-export function GET({ url, request }) {
+import { existsSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
+import { join } from 'path';
+
+export async function GET({ url, request }) {
 	const media = new URL(url).searchParams.get('url');
 	if (!media) {
 		return new Response('No media URL provided', { status: 400 });
 	}
 	const mediaURL = new URL(media);
-	const headers = new Headers(request.headers);
-	headers.set('referer', 'https://www.instagram.com/');
-	headers.set('origin', 'https://www.instagram.com');
-	headers.set('host', mediaURL.host);
-	headers.set('sec-fetch-dest', 'image');
-	headers.set('sec-fetch-mode', 'cors');
-	headers.set('sec-fetch-site', 'cross-site');
-	return fetch(mediaURL, { headers });
+
+	const fileName = mediaURL.pathname.split('/').pop();
+	if (!fileName) {
+		return new Response('Invalid media URL', { status: 400 });
+	}
+
+	// To avoid hitting the Instagram API suspiciously, I cache media on disk.
+	const file = join('/Users/nchiang/repos/vision/data/media', fileName);
+
+	if (!existsSync(file)) {
+		console.log('Downloading media', mediaURL);
+		const headers = new Headers(request.headers);
+		headers.set('referer', 'https://www.instagram.com/');
+		headers.set('origin', 'https://www.instagram.com');
+		headers.set('host', mediaURL.host);
+		headers.set('sec-fetch-dest', 'image');
+		headers.set('sec-fetch-mode', 'cors');
+		headers.set('sec-fetch-site', 'cross-site');
+
+		const arrayBuffer = await fetch(mediaURL, { headers })
+			.then((response) => response.blob())
+			.then((blob) => blob.arrayBuffer());
+		await writeFile(file, Buffer.from(arrayBuffer));
+	}
+
+	console.log('Found existing media', file);
+	const buffer = await readFile(file);
+	return new Response(buffer);
 }
